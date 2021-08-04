f055a5342a6786e5840b62add1ae916fb46c495c
Qodir Masruri <qodir.bvk@gmail.com>
2021-08-04 22:00:44 +0700

    ADD: Create podfile

---
+++ HeroDota/Pods/Target Support Files/Pods-HeroDota-HeroDotaUITests/Pods-HeroDota-HeroDotaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeroDota_HeroDotaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HeroDota_HeroDotaUITests
+@end
---
+++ HeroDota/Pods/Target Support Files/Pods-HeroDota/Pods-HeroDota-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeroDota : NSObject
+@end
+@implementation PodsDummy_Pods_HeroDota
+@end
